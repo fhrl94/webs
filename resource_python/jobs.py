32509bd4599cf28c1dea0bf096147637fea7fa2e
fhrl94 <445695121@qq.com>
2018-02-02 10:03:37 +0800

    Linux定时任务 使用 uWSGI 中的 mule 实现 修复

--- resource_python/jobs.py
+++ resource_python/jobs.py
@@ -19,3 +19,12 @@ register_events(scheduler)
 
 scheduler.start()
 print("Scheduler started!")
+
+## 下面这句加在定时任务模块的末尾...判断是否运行在uwsgi模式下, 然后阻塞mule主线程(猜测).
+try:
+    import uwsgi
+    while True:
+        sig = uwsgi.signal_wait()
+        print(sig)
+except Exception as err:
+    pass
